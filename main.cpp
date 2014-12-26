be6341ca7745fdab14ea6e49bc51444cd225fe05
Steve Cobb <scobb1@utexas.edu>
2014-12-25 22:27:08 -0600

    Now accepts command line argument.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
-int main() {
-    string local_path = "/Users/scobb/ClionProjects/Algo_PA1/";
-    string in_file_path = local_path + "small/input0.in";
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " IN_FILE" << endl;
+        return 1;
+    }
+    string in_file_path = argv[1];
     Parser myParser = Parser(in_file_path);
     myParser.process();
 
